5d2c83d160df324c9526273e65a9b9a0b68ee933
Alexander Palekov <palekov-2011@mail.ru>
2020-02-11 12:08:11 +0300

    Spring Boot GB webinar

---
+++ src/test/java/ru/palekov/SpringBootTut/SpringBootTutApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.palekov.SpringBootTut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
